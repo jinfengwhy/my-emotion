e8f0ff9c05d1588ae40cc997d6658bbb70dc59d4
jinfengw <wangjinfeng@linctex.com>
2023-08-15 21:27:41 +0800

    feat: 一周曲线雏形

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      <h2>情绪曲线</h2>
       <Outlet />
     </div>
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,24 @@
 import * as React from "react";
-import { useRoutes } from "react-router-dom";
+import { useRoutes, Navigate } from "react-router-dom";
 
 import App from '../App';
 import Test from '../pages/test'
 import NotFound from '../pages/notfound'
+import Dashboard from '../pages/dashboard'
 
 const routes = [
   {
     path: '/',
     element: <App />,
     children: [
+      {
+        path: '/',
+        element: <Navigate to="/dashboard" />
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard />
+      },
       {
         path: 'test',
         element: <Test />
